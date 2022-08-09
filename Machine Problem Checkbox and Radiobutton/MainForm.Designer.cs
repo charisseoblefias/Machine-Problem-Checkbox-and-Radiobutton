26cba698fdd5db906fd229a8a988945dceacc7f8
char-kyeopta <charisse.oblefias01@gmail.com>
2022-08-09 12:10:42 +0800

    Add project files.

---
+++ Machine Problem Checkbox and Radiobutton/MainForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Charisse Oblefias
+ * Date: 5/15/2021
+ * Time: 6:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Machine_Problem_Checkbox_and_Radiobutton
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// MainForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "Machine Problem Checkbox and Radiobutton";
+			this.Name = "MainForm";
+		}
+	}
+}
---
+++ Machine Problem Checkbox and Radiobutton/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Charisse Oblefias
+ * Date: 5/15/2021
+ * Time: 6:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Machine_Problem_Checkbox_and_Radiobutton
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
